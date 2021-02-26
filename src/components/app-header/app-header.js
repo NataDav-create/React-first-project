1fb889553356a365da7a904b101e8d00fe9e792c
Natalia Davydova <davydovanatalia063@gmail.com>
2021-02-26 14:31:08 -0800

    add folder and files app, folder and files app-header, add folder and file post-add-form, add folder and file post-list, add folder and file post-list-item, add folder and file post-status-filter, add folder and file search-panel, add markup for future project

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app-header.css";
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1> Natalia Davydova </h1> <h5> 5 posts, add it was liked 0 </h5>{" "}
+    </div>
+  );
+};
+
+export default AppHeader;
